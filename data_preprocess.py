d1415aaa642859fab9302dbf114eee055c79aac2
Yuerong Zhang <yuerongzhang2017@outlook.com>
2022-04-27 11:45:05 -0400

    update README

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,37 +1,42 @@
 import csv
+
+
 # our data source: @ NYC OPEN DATA
 # link to original data: https://data.cityofnewyork.us/NYC-BigApps/NYPD-Motor-Vehicle-Collisions-Summary/m666-sf2m
 
 def is_recent(row):
     year = int(row[0].split('/')[2])
-    return year >= 2022 # For quick check
+    return year >= 2022  # For quick check
     # return year >= 2020
+
 
 def get_crush_season(row):
     month = int(row[0].split('/')[0])
-    if month >= 3 and month <=5:
+    if 3 <= month <= 5:
         return 'spring'
-    elif month >=6 and month <= 8:
+    elif 6 <= month <= 8:
         return 'summer'
-    elif month >=9 and month <= 11:
+    elif 9 <= month <= 11:
         return 'autumn'
     else:
         return 'winter'
 
+
 def get_crush_time(row):
     hour = int(row[1].split(':')[0])
-    if hour >= 3 and hour <= 6:
+    if 3 <= hour <= 6:
         return 'dawn'
-    elif hour >= 7 and hour <= 10:
+    elif 7 <= hour <= 10:
         return 'morning'
-    elif hour >= 11 and hour <= 14:
+    elif 11 <= hour <= 14:
         return 'noon'
-    elif hour >= 15 and hour <= 18:
+    elif 15 <= hour <= 18:
         return 'afternoon'
-    elif hour >= 19 and hour <= 22:
+    elif 19 <= hour <= 22:
         return 'night'
-    elif hour >= 23 or hour <= 2:
+    elif 23 <= hour or hour <= 2:
         return 'midnight'
+
 
 def get_crush_borough(row):
     borough = row[2].strip()
@@ -39,6 +44,7 @@ def get_crush_borough(row):
         return borough.lower().replace(' ', '_').replace('/', '_')
     else:
         return 'unspecified_borough'
+
 
 # def getZip(row):
 #     row[3] = row[3].strip()
@@ -54,13 +60,14 @@ def get_crush_street(row):
     else:
         return 'unspecified_street'
 
+
 def get_person_status(row):
     data = []
 
-    if int(row[12])>0 or int(row[14])>0 or int(row[16])>0:
+    if int(row[12]) > 0 or int(row[14]) > 0 or int(row[16]) > 0:
         data.append('injured')
 
-    if int(row[13])>0 or int(row[15])>0 or int(row[17])>0:
+    if int(row[13]) > 0 or int(row[15]) > 0 or int(row[17]) > 0:
         data.append('killed')
 
     if len(data) == 0:
@@ -68,23 +75,26 @@ def get_person_status(row):
 
     return data
 
+
 def get_crush_reason(row):
     data = set()
     is_unspecified = 1
     r1 = row[18].strip()
     r2 = row[19].strip()
     r3 = row[20].strip()
+
     if r1 != 'Unspecified' and r1 != '':
         is_unspecified = 0
-        data.add(r1.lower().replace(' ', '_').replace('/', '_').replace('-', '_')) 
+        data.add(r1.lower().replace(' ', '_').replace('/', '_').replace('-', '_'))
     if r2 != 'Unspecified' and r2 != '':
         is_unspecified = 0
-        data.add(r2.lower().replace(' ', '_').replace('/', '_').replace('-', '_')) 
+        data.add(r2.lower().replace(' ', '_').replace('/', '_').replace('-', '_'))
     if r3 != 'Unspecified' and r3 != '':
         is_unspecified = 0
-        data.add(r3.lower().replace(' ', '_').replace('/', '_').replace('-', '_')) 
+        data.add(r3.lower().replace(' ', '_').replace('/', '_').replace('-', '_'))
     if is_unspecified == 1:
         data.add('unspecified_reason')
+
 
 def get_vehicle_type(row):
     data = set()
@@ -145,7 +155,6 @@ with open('/Users/test/Desktop/6111/Motor_Vehicle_Collisions_-_Crashes.csv', new
             data.extend(vehicle_list)
 
             filtered_data.append(data)
-
 
 with open('INTEGRATED-DATASET.csv', mode='w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
